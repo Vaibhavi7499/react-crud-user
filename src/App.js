06652d2bfcde72db1e95c1bc09f4054a60097248
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2024-08-14 14:28:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -18,31 +18,31 @@ import Todo from "./Todo";
 
 function App() {
   return (
-    // <div className="App">
-    //   <BrowserRouter>
-    //     <Layout />
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/about" element={<About />} />
-    //       <Route path="/adduser" element={<AddUser />} />
-    //       <Route path="/adduser/:id" element={<AddUser />} />
-    //       <Route path="/viewuser/:id" element={<ViewUser />} />
-    //       <Route path="/emphome" element={<HomeEmp />} />
-    //       <Route path="/addemp" element={<AddEmp />} />
-    //       <Route path="/addemp/:id" element={<AddEmp />} />
-    //       <Route path="/viewemp/:id" element={<ViewEmp />} />
-    //       <Route path="course" element={<Course />}>
-    //         <Route path="search" element={<Search />}></Route>
-    //         <Route path="list" element={<List />}></Route>
-    //       </Route>
-    //       <Route path="nestedhome" element={<NestedHome />}>
-    //         <Route path="nestedabout" element={<NestedAbout />}></Route>
-    //       </Route>
-    //     </Routes>
-    //   </BrowserRouter>
-    // </div>
+    <div className="App">
+      <BrowserRouter>
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/adduser" element={<AddUser />} />
+          <Route path="/adduser/:id" element={<AddUser />} />
+          <Route path="/viewuser/:id" element={<ViewUser />} />
+          <Route path="/emphome" element={<HomeEmp />} />
+          <Route path="/addemp" element={<AddEmp />} />
+          <Route path="/addemp/:id" element={<AddEmp />} />
+          <Route path="/viewemp/:id" element={<ViewEmp />} />
+          <Route path="course" element={<Course />}>
+            <Route path="search" element={<Search />}></Route>
+            <Route path="list" element={<List />}></Route>
+          </Route>
+          <Route path="nestedhome" element={<NestedHome />}>
+            <Route path="nestedabout" element={<NestedAbout />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
     // <CssPractise></CssPractise>
-    <Todo></Todo>
+    // <Todo></Todo>
   );
 }
 
